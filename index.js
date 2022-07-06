9db1fbb4eb234fa29be12f74b57124818f006084
Luke Ellis <lke8@cornell.edu>
2022-07-05 20:28:30 -0400

    File Uploads working

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 require('dotenv').config()
 const { MongoClient, ObjectID } = require("mongodb")
 const express = require("express")
-const BodyParser = require('body-parser')
-const { response } = require("express")
+// const BodyParser = require('body-parser')
+// const { response } = require("express")
 const path = require('path')
 const { appendFile } = require("fs")
+const multer = require("multer")
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "public/images/uploads")
+    },
+    filename: (req, file, cb) => {
+        console.log(file)
+        cb(null, Date.now() + path.extname(file.originalname))
+    }
+})
+const upload = multer({ storage: storage})
 
 const server = express()
 
-server.use(BodyParser.json())
-server.use(BodyParser.urlencoded({ extended: true }))
+// server.use(BodyParser.json())
+// server.use(BodyParser.urlencoded({ extended: true }))
 
 server.set('views', "./views")
 server.set('view engine', "ejs")
@@ -17,6 +28,7 @@ server.set('view engine', "ejs")
 server.use(express.static('public'))
 server.use('/images', express.static(path.join(__dirname, 'public', 'images')))
 server.use('/css', express.static(path.join(__dirname, 'public', 'styles')))
+
 
 const client = new MongoClient(process.env.LUKE_ATLAS_URI)
 const port = process.env.PORT || 3000
@@ -35,13 +47,18 @@ server.listen(port, async () => {
 })
 
 // UPLOAD DATA
-server.post("/submit-art", async (req, res) => {
+server.post("/submit-art", upload.single("image"), async (req, res) => {
     try {
         console.log(req.body)
         let result = await artCollection.insertOne(req.body)
-        // res.send(result)
-        res.render("entry-result.ejs", {result: result})
+        const new_id = result.insertedId
+        let file_add = await artCollection.updateOne(
+            {"_id": new_id},
+            { $set: {"filename": req.file.filename}}
+        )
+        res.render("entry-result.ejs", {result: result, file_result: file_add})
     } catch (e) {
+        console.log(e)
         res.status(500).send("DIDN'T WORK :(")
     }
 })
@@ -63,7 +80,7 @@ server.get("/all_art", async (req, res) => {
     try {
         console.log("All records have been requested!")
         let result = await artCollection.find({}).toArray()
-        res.render("phone-gallery.ejs", {results: result})
+        res.render("allrecords.ejs", {results: result})
     } catch (e) {
         res.status(500).send("FAILURE")
     }
