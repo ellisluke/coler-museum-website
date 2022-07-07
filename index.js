c1875820fadaa7a7134331176bfc0cceddf52dd5
Luke Ellis <lke8@cornell.edu>
2022-07-06 22:58:59 -0400

    login works!

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 require('dotenv').config()
-const { MongoClient, ObjectID } = require("mongodb")
+const { MongoClient, ObjectID, MongoUnexpectedServerResponseError } = require("mongodb")
 const express = require("express")
-// const BodyParser = require('body-parser')
-// const { response } = require("express")
+const BodyParser = require('body-parser')
+const { response } = require("express")
 const path = require('path')
 const { appendFile } = require("fs")
 const multer = require("multer")
@@ -19,8 +19,16 @@ const upload = multer({ storage: storage})
 
 const server = express()
 
-// server.use(BodyParser.json())
-// server.use(BodyParser.urlencoded({ extended: true }))
+// Login dependencies
+const bcrypt = require('bcrypt')
+const passport = require('passport')
+const session = require('express-session')
+const flash = require("express-flash")
+
+
+
+server.use(BodyParser.json())
+server.use(BodyParser.urlencoded({ extended: true }))
 
 server.set('views', "./views")
 server.set('view engine', "ejs")
@@ -29,17 +37,26 @@ server.use(express.static('public'))
 server.use('/images', express.static(path.join(__dirname, 'public', 'images')))
 server.use('/css', express.static(path.join(__dirname, 'public', 'styles')))
 
+server.use(session({
+    secret: "aqu457mcf06$%^&@",
+    resave: false,
+    saveUninitialized: false,
+    cookie: { maxAge: 60000 }
+}))
 
 const client = new MongoClient(process.env.LUKE_ATLAS_URI)
 const port = process.env.PORT || 3000
 var artCollection
 var galleryCollection
+var userCollection
+
 
 server.listen(port, async () => {
     try {
         await client.connect()
         artCollection = client.db("coler-museum").collection("artwork")
         galleryCollection = client.db("coler-museum").collection("gallery-data")
+        userCollection = client.db("coler-museum").collection("users")
         console.log("Listening at port: " + port)
     } catch (e) {
         console.error(e)
@@ -53,6 +70,47 @@ server.get("/", (req, res) => {
 
 server.get("/galleries", (req, res) => {
     res.render("galleries.ejs")
+})
+
+server.get('/login', (req, res) => {
+    res.render("login.ejs", {message: ""})
+})
+
+server.post('/login', async (req, res) => {
+    try {
+        let userData = await userCollection.findOne()
+        req.session.admin = false
+        if (req.body.username == userData.username) {
+            bcrypt.compare(req.body.password, userData.password, (err, result) => {
+                if (result) {
+                    req.session.admin = true
+                    res.redirect("/admin")
+                }
+            })
+        } else {
+            res.render("login.ejs", {message: "Login attempt failed."})
+        }
+        
+        
+        // if (req.body.username == userData.username && req.body.password == userData.password) {
+        //     res.send("success!")
+        // } else {
+        //     res.send("failure!")
+        // }
+    } catch (e) {
+        res.send("uh oh")
+    }
+})
+
+// ADMIN PAGE
+server.get("/admin", async (req, res) => {
+    console.log(req.session.admin)
+    if (!req.session.admin) {
+        res.redirect('/')
+    }
+    else {
+        res.render("admin.ejs")
+    }
 })
 
 // UPLOAD DATA
@@ -106,6 +164,8 @@ server.get("/gallery-data", async(req, res) => {
 })
 
 server.get("/enter-art", async (req, res) => {
-    console.log("Going to submit new art...")
+    if (!req.session.admin) {
+        res.redirect('/')
+    }
     res.render("art-entry-form.ejs")
 })
